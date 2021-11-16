1f130a0c168024e9af9d6fda345621df3c1696ea
floflx7 <82363226+floflx7@users.noreply.github.com>
2021-11-16 18:39:08 +0100

    form  CreatPost

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Link to="/createpost"> Create A Post</Link>
+        <Link to="/"> Home Page</Link>
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/createpost" exact component={CreatePost} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
