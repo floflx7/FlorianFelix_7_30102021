6e3704ef64bb11f1bbc4896edfed01496f4b0b95
floflx7 <82363226+floflx7@users.noreply.github.com>
2021-11-16 15:14:42 +0100

    sequelize install

---
+++ server/models/Post.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+  const Posts = sequelize.define("Posts", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return Posts;
+};
