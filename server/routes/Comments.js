7f89a5113fe75420f90d728672f99a790e6091b6
floflx7 <82363226+floflx7@users.noreply.github.com>
2021-11-18 18:06:18 +0100

    token fonctionnels

--- server/routes/Comments.js
+++ server/routes/Comments.js
@@ -1,7 +1,7 @@
-const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const { Comments } = require("../models");
+const { validateToken } = require("../middlewares/AuthMiddleware");
 
 router.get("/:postId", async (req, res) => {
   const postId = req.params.postId;
@@ -9,8 +9,10 @@ router.get("/:postId", async (req, res) => {
   res.json(comments);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", validateToken, async (req, res) => {
   const comment = req.body;
+  const username = req.user.username;
+  comment.username = username;
   await Comments.create(comment);
   res.json(comment);
 });
